25e0c39bfae4e08d021e394c1c005721ab51acf9
Alex Fink <alexanderjfink@gmail.com>
2014-01-25 11:27:00 -0600

    Wizard: Next Step button functionality works

--- public/javascripts/apps/wizard/views/footer.js
+++ public/javascripts/apps/wizard/views/footer.js
@@ -2,7 +2,50 @@ define(['app','../templates'], function (UniversalDesignBootstrap, templates) {
 	UniversalDesignBootstrap.module('WizardModule.Views', function(Views, ContactManager, Backbone, Marionette, $, _){
 
 		Views.Footer = Marionette.ItemView.extend({
-			template : templates.footer
+			template : templates.footer,
+
+			ui: {
+
+			},
+
+			events: {
+				'click .add-another': 'onButtonPressAddAnother',
+				'click .next-step': 'onButtonPressNextStep'
+			},
+
+			initialize: function(options) {
+				var options = options || {};
+				
+				this.addAnother = this.addAnother || false;
+			},
+
+			// fake model
+			serializeData: function(){
+				return {
+					addAnother: this.addAnother
+				};
+			},
+
+
+			// ui methods
+			onButtonPressAddAnother: function(evt) {
+				this.addAnother();
+			},
+
+			onButtonPressNextStep: function(evt) {
+				this.showNextStep();
+			},
+
+			// methods
+			addAnother: function() {
+
+			},
+
+			showNextStep: function() {
+				currentStepNumber = UniversalDesignBootstrap.getCurrentRoute().split(['/'])[1];
+
+				UniversalDesignBootstrap.trigger("WizardModule:step:showStep", (parseInt(currentStepNumber) + 1));
+			}
 		});
 
 	});
